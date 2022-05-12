8029d644245f9dac6dce83c14f0e70a2555a95fe
Aldo Fabro <aldocfabro@gmail.com>
2022-05-12 12:15:22 -0300

    change theme app

--- src/App.vue
+++ src/App.vue
@@ -57,7 +57,7 @@ onMounted(async () => {
 </script>
 
 <style>
-@import 'primevue/resources/themes/tailwind-light/theme.css';
+@import 'primevue/resources/themes/lara-light-teal/theme.css';
 @import 'primevue/resources/primevue.min.css';
 @import 'primeicons/primeicons.css';
 @import '/node_modules/primeflex/primeflex.css';
